b555007f2dc5ba806eca7505da2b2c575e97f6b4
roshni090496 <rs09041001@gmail.com>
2022-04-01 14:43:07 -0700

    Area program Added

--- day5assgn.sh
+++ day5assgn.sh
@@ -1,3 +1,11 @@
 #Sequences Practice Problems
 echo "Single Digit Random No:"
 echo $((RANDOM%10))
+
+#Area Conversion
+
+read -p "Please Enter Height in feet = " H
+read -p "Please Enter Widht in feet  = " W
+Foot=$((H*W))
+mt=$((Foot/3))
+echo "Area of Rectangle in meter is  " $mt
